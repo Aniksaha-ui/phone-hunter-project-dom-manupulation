2ba258df6ae35a261c6ba2f973305111f4081ad0
Aniksaha-ui <sahaanik1045@gmail.com>
2022-03-01 02:38:32 +0600

    phone details show without sensors part

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//load phones
 const loadPhones = () => {
   const searchField = document.getElementById("phone-input");
   const searchValue = searchField.value;
@@ -7,9 +8,11 @@ const loadPhones = () => {
   const url = ` https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
   fetch(url)
     .then((res) => res.json())
-    .then((data) => displayPhone(data.data));
+    .then((data) => displayPhone(data.data))
+    .catch((error) => console.log(error));
 };
 
+//display phones
 const displayPhone = (phones) => {
   const searchResultField = document.getElementById("search-result");
   searchResultField.textContent = "";
@@ -39,4 +42,36 @@ const displayPhone = (phones) => {
 
     searchResultField.appendChild(div);
   });
+};
+
+//selected phone details load
+const phoneDetailsBySlug = (slug) => {
+  const url = `https://openapi.programming-hero.com/api/phone/${slug}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayPhoneDetails(data.data))
+    .catch((error) => console.log(error));
+};
+
+//selected phone details show
+const displayPhoneDetails = (phone) => {
+  const phoneDetails = document.getElementById("phone-details");
+  phoneDetails.textContent = "";
+  const div = document.createElement("div");
+  div.classList.add("card");
+  div.innerHTML = `
+    <div class="d-flex align-items-center justify-content-center">
+        <img src="${phone.image}" class="card-img-top w-50" alt="..." />
+    </div>
+    <div class="card-body">
+      <h5 class="card-title text-center">${phone.name}</h5>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item text-center">Relase Date:${phone.releaseDate}</li>
+      <li class="list-group-item text-center">Brand:${phone.brand}</li>
+      <li class="list-group-item text-center">Main Features: ${phone.mainFeatures.storage},${phone.mainFeatures.displaySize},${phone.mainFeatures.chipSet},${phone.mainFeatures.memory}</li>
+    </ul>
+  `;
+
+  phoneDetails.appendChild(div);
 };
