b2724c28e899d77105a0c916df334aed06d454ba
Aniksaha-ui <sahaanik1045@gmail.com>
2022-03-01 10:58:13 +0600

    error handleing done if there is no data after search

--- js/app.js
+++ js/app.js
@@ -14,34 +14,40 @@ const loadPhones = () => {
 
 //display phones
 const displayPhone = (phones) => {
-  const searchResultField = document.getElementById("search-result");
-  searchResultField.textContent = "";
-  phones.slice(0, 20).forEach((phone) => {
-    const div = document.createElement("div");
-    div.classList.add("col");
-    div.innerHTML = `
-    <div class="card p-1" >
-    <div class="d-flex align-items-center justify-content-center">
-        <img src="${phone.image}" class="card-img-top w-75" alt="..." />
-    </div>
-  
-    <div class="card-body">
-      <h5 class="card-title text-center">${phone.slug}</h5>
+  if (phones.length == 0) {
+    alert("No data Found");
+  }
+  // console.log(phones);
+  else {
+    const searchResultField = document.getElementById("search-result");
+    searchResultField.textContent = "";
+    phones?.slice(0, 20).forEach((phone) => {
+      const div = document.createElement("div");
+      div.classList.add("col");
+      div.innerHTML = `
+      <div class="card p-1" >
+      <div class="d-flex align-items-center justify-content-center">
+          <img src="${phone.image}" class="card-img-top w-75" alt="..." />
+      </div>
     
+      <div class="card-body">
+        <h5 class="card-title text-center">${phone.slug}</h5>
+      
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item text-center">phone Name:${phone.phone_name}</li>
+        <li class="list-group-item text-center">Brand:${phone.brand}</li>
+        <li class="list-group-item text-center">
+           <button onclick="phoneDetailsBySlug('${phone.slug}')" class="btn btn-success">Details</button>
+        </li>
+       
+      </ul>
     </div>
-    <ul class="list-group list-group-flush">
-      <li class="list-group-item text-center">phone Name:${phone.phone_name}</li>
-      <li class="list-group-item text-center">Brand:${phone.brand}</li>
-      <li class="list-group-item text-center">
-         <button onclick="phoneDetailsBySlug('${phone.slug}')" class="btn btn-success">Details</button>
-      </li>
-     
-    </ul>
-  </div>
-    `;
+      `;
 
-    searchResultField.appendChild(div);
-  });
+      searchResultField.appendChild(div);
+    });
+  }
 };
 
 //selected phone details load
@@ -70,6 +76,11 @@ const displayPhoneDetails = (phone) => {
       <li class="list-group-item text-center">Relase Date:${phone.releaseDate}</li>
       <li class="list-group-item text-center">Brand:${phone.brand}</li>
       <li class="list-group-item text-center">Main Features: ${phone.mainFeatures.storage},${phone.mainFeatures.displaySize},${phone.mainFeatures.chipSet},${phone.mainFeatures.memory}</li>
+     
+      <li class="list-group-item text-center">sensors:${phone?.mainFeatures.sensors}</li>
+      
+
+
     </ul>
   `;
 
